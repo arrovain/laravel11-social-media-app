823eb178b744114078f62e692816c25cf079aece
Mahmut İÇME <arrovain@gmail.com>
2024-08-16 14:50:50 +0300

    master and nav views

---
+++ resources/views/master.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="http://cdn.gjsdelivr.net.com/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+ 
+    
+
+
+<script src="http://cdn.gjsdelivr.net.com/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" ></script>
+
+</body>
+</html>
