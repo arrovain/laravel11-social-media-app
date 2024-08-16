1d1716e0e65e6715cc33627722a0cf029b2e6218
Mahmut İÇME <arrovain@gmail.com>
2024-08-16 14:55:34 +0300

    include nav to the master

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -5,14 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link href="http://cdn.gjsdelivr.net.com/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="http://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
- 
+ @include('template.nav')
     
 
 
-<script src="http://cdn.gjsdelivr.net.com/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" ></script>
+<script src="http://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" ></script>
 
 </body>
 </html>
