28edaf075ddf77816e075ceb83ad924a36b85a58
Mahmut İÇME <arrovain@gmail.com>
2024-08-16 14:31:46 +0300

    posts table

--- database/migrations/2024_08_16_112117_create_posts_table.php
+++ database/migrations/2024_08_16_112117_create_posts_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('image');
+            $table->string('description');
+            $table->unsignedBigInteger('user_id')->nullable(true);
+            $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
         });
     }
